9f2b37d8b9d362ff782b26b9cca13c4e49b63a04
josebeap <josebeap99@gmail.com>
2022-12-11 23:43:40 -0500

    Diseño grafico

---
+++ Vista/main.py
@@ -0,0 +1,84 @@
+from tkinter import *
+#from Sistema.Graph import Graph
+#from Sistema.Vertex import Vertex
+
+
+class main:
+    def __init__(self):
+        self.app = Tk()
+        self.app.title('Procesador')
+        self.app.state('zoomed')
+
+        #self.g = Graph()
+        #self.city = None
+
+
+        self.alu = PhotoImage(file="../Imagenes/alu.png")
+        self.mar = PhotoImage(file="../Imagenes/MAR.png")
+        self.ir = PhotoImage(file="../Imagenes/IR.png")
+        self.mbr = PhotoImage(file="../Imagenes/MBR.png")
+        self.pc = PhotoImage(file="../Imagenes/PC.png")
+        self.uc = PhotoImage(file="../Imagenes/UC.png")
+        self.ac = PhotoImage(file="../Imagenes/AC.png")
+        #self.donkey = PhotoImage(file="../Images/donkey1.png")
+        #self.plane = PhotoImage(file="../Images/plane.png")
+        #self.car = PhotoImage(file="../Images/car.png")
+        #self.cityIcon = PhotoImage(file="../Images/city1.png")
+        #self.donkeyDead = PhotoImage(file="../Images/donkeyDead.png")
+        #self.backParker = PhotoImage(file="../Images/mochilero.png")
+
+        self.frame = Frame(self.app)
+        self.frame.pack(fill="both")
+        self.frame.config(width=1360, height=760, bg='navy')
+
+        self.entry2 = StringVar()
+        self.textField2 = Entry(self.frame, textvariable=self.entry2).place(x=10, y=30)
+
+        self.entry3 = StringVar()
+        self.textField3 = Entry(self.frame, textvariable=self.entry3).place(x=10, y=60)
+
+        self.button2 = Button(
+            button2=Button(self.frame, text="Cargar Datos", command="self.drawElement").place(x=10, y=90))
+
+
+        self.button4 = Button(button4=Button(self.frame, text="Suma", command="self.transportSelection").place(x=10, y=200))
+        self.button5 = Button(button5=Button(self.frame, text="Resta", command="self.transportSelection2").place(x=10, y=230))
+        self.button6 = Button(button6=Button(self.frame, text="Multiplicación", command="self.transportSelection3").place(x=10, y=260))
+        self.button7 = Button(button7=Button(self.frame, text="División", command="self.timeTravel").place(x=10, y=290))
+        self.button8 = Button(button8=Button(self.frame, text="Comparar", command="self.timeTravel").place(x=10, y=320))
+
+
+
+
+        self.map = Canvas(self.frame, width=1360, height = 760, bg='grey')
+        self.map.place(x=250, y=0)
+
+
+        #self.map.create_rectangle(800, 400, 1000, 300, fill="indian red")
+
+        self.map.create_image(10, 200, anchor=NW, image=self.alu)
+        self.map.create_text(250, 530, fill="black", font="Times 30", text="ALU")
+
+        self.map.create_image(30, 30, anchor=NW, image=self.mar)
+        self.map.create_text(100, 100, fill="black", font="Times 30", text="MAR")
+
+        self.map.create_image(350, 30, anchor=NW, image=self.ir)
+        self.map.create_text(425, 100, fill="black", font="Times 30", text="IR")
+
+        self.map.create_image(30, 610, anchor=NW, image=self.mbr)
+        self.map.create_text(100, 680, fill="black", font="Times 30", text="MBR")
+
+        self.map.create_image(700, 200, anchor=NW, image=self.pc)
+        self.map.create_text(770, 270, fill="black", font="Times 30", text="PC")
+
+        self.map.create_image(700, 350, anchor=NW, image=self.uc)
+        self.map.create_text(770, 420, fill="black", font="Times 30", text="UC")
+
+        self.map.create_image(700, 500, anchor=NW, image=self.ac)
+        self.map.create_text(770, 570, fill="black", font="Times 30", text="AC")
+
+        #self.openFile()
+        self.app.mainloop()
+
+
+main()
