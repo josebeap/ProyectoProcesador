9ffe913ea6f032f6516e21ff2df50e3612a220da
Nicolas Estupiñan Jaime <nico.ej99@gmail.com>
2022-12-13 15:56:44 -0500

    metodos clase Alu

--- Entidades/Alu.py
+++ Entidades/Alu.py
@@ -1,1 +1,25 @@
 
+class Alu:
+    def __init__(self):
+        self.operando1 = 0
+        self.operando2 = 0
+        self.operacion = 0
+
+
+    def Operar(self,simbolo):
+        if(simbolo == "-"):
+            self.operacion = self.operando1 - self.operando2
+        elif(simbolo == "*"):
+            self.operacion = self.operando1 * self.operando2
+        elif(simbolo == "/"):
+            self.operacion = self.operando1 / self.operando2
+        elif(simbolo == "+"):
+            self.operacion = self.operando1 + self.operando2
+        else:
+            print("No es operacion valida")
+    def setOperando1(self,numero):
+        self.operando1 = numero
+    def setOperando2(self,numero):
+        self.operando2 = numero
+    def getResultado(self):
+        return self.operacion
