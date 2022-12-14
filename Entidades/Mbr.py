5e4660a5aa6ff6e651c0074ca6ac8105b9314f34
Nicolas Estupiñan Jaime <nico.ej99@gmail.com>
2022-12-13 21:03:54 -0500

    Implementar metodos en Mbr, Uc

---
+++ Entidades/Mbr.py
@@ -0,0 +1,10 @@
+
+class Mbr:
+    def __init__(self):
+        self.datoCargado = ""
+
+    def getDato(self):
+        return self.datoCargado
+
+    def setDato(self, dato):
+        self.datoCargado = dato
---
+++ Entidades/Uc.py
@@ -0,0 +1,12 @@
+import Ir, Pc, Memoria, Alu
+class Uc:
+
+    def __init__(self):
+        self.pilaOperandos = []
+        self.registroInstrucciones = Ir
+        self.contradorPrograma = Pc
+        self.memoriaPrincipal = Memoria
+        self.alu = Alu
+
+    
+
